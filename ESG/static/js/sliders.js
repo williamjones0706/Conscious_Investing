d55c005cfcfccb78ba0bfa0e83e60b33de5e9267
yorchd3 <49838844+yorchd3@users.noreply.github.com>
2020-03-07 13:49:21 -0500

    updating recommendations

---
+++ ESG/static/js/sliders.js
@@ -0,0 +1,80 @@
+function modifyOffset() {
+    var el, newPoint, newPlace, offset, siblings, k;
+    width    = this.offsetWidth;
+    newPoint = (this.value - this.getAttribute("min")) / (this.getAttribute("max") - this.getAttribute("min"));
+    offset   = -5;
+    if (newPoint < 0) { newPlace = 0;  }
+    else if (newPoint > 1) { newPlace = width; }
+    else { newPlace = width * newPoint + offset; offset -= newPoint;}
+    siblings = this.parentNode.childNodes;
+    for (var i = 0; i < siblings.length; i++) {
+        sibling = siblings[i];
+        if (sibling.id == this.id) { k = true; }
+        if ((k == true) && (sibling.nodeName == "OUTPUT")) {
+            outputTag = sibling;
+        }
+    }
+    outputTag.style.left       = newPlace + "px";
+    outputTag.style.marginLeft = offset;
+    outputTag.innerHTML        = this.value;
+    // console.log(this.value);
+}
+
+function modifyInputs() {
+    
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute("type") == "range") {
+            inputs[i].onchange = modifyOffset;
+            
+
+            // the following taken from http://stackoverflow.com/questions/2856513/trigger-onchange-event-manually
+            if ("fireEvent" in inputs[i]) {
+                inputs[i].fireEvent("onchange");
+            } else {
+                var evt = document.createEvent("HTMLEvents");
+                evt.initEvent("change", false, true);
+                inputs[i].dispatchEvent(evt);
+
+            }
+        }
+    }
+}
+
+// print(evt)
+
+
+async function myfunction(event) {
+    event.preventDefault()
+    let json={
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+        data: {
+          E: envOUT.value,
+          S: socOUT.value,
+          G: govOUT.value,        
+        }
+    }
+    
+}
+
+
+// console.log(envOUT.value);
+// console.log(socOUT.value);
+// console.log(govOUT.value);
+
+function myfunction() {
+    console.log(envOUT.value);
+    console.log(socOUT.value);
+    console.log(govOUT.value);
+}
+
+modifyInputs();
+// myfunction();
