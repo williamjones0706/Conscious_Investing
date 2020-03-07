f7bcbbe35416c14c1045d4c9252377b5f7cae015
William Jones <william.jones0706@gmail.com>
2020-03-07 01:25:23 -0500

    updating html files for front end

--- ESG/static/js/stock_perf_2019_funnel.js
+++ ESG/static/js/stock_perf_2019_funnel.js
@@ -1,10 +1,22 @@
 var gd = document.getElementById('funnel');
 var data = [
-    {type: 'funnel', 
-    y: ["All Stocks", "> 0% Returns", "> 10% Returns", "> 20% Returns", "> 40% Returns", "> 60% Returns", "> 90% Returns", "> 110% Returns"], 
-    x: [724, 623, 539, 421, 181, 61, 12, 4], 
-    hoverinfo: 'x+percent previous+percent initial'}];
+    {
+        type: 'funnel',
+        y: ["<b>All Stocks</b>", "<b>> 0% Returns</b>", "<b>> 10% Returns</b>", "<b>> 20% Returns</b>", "<b>> 40% Returns</b>", 
+        "<b>> 60% Returns</b>", "<b>> 90% Returns</b>", "<b>> 110% Returns</b>"],
+        x: [724, 623, 539, 421, 181, 61, 12, 4],
+        hoverinfo: 'x+percent previous+percent initial'
+    }];
 
-var layout = {margin: {l: 150}, width:600, height: 700}
+var layout = {
+    margin: {
+        l: 150,
+        r: 150,
+        t: 0,
+        b: 0
+    },
+    width: 1110,
+    height: 400
+}
 
 Plotly.newPlot('funnel', data, layout);
